c520b2c97ec74f93063d17913986b7e3652d6bb0
Motaz <motazyusuf@gmail.com>
2024-08-07 01:26:18 +0300

    Settings provider for color Mode

--- lib/modules/settings/settings_component.dart
+++ lib/modules/settings/settings_component.dart
@@ -37,6 +37,7 @@ class _SettingsComponentState extends State<SettingsComponent> {
           Padding(
             padding: const EdgeInsets.only(left: 15.0, top: 5),
             child: CustomDropdown<String>(
+              initialItem: widget.options[0],
               closedHeaderPadding: EdgeInsets.all(10),
               hintText: "Select ${widget.optionsName}",
               decoration: CustomDropdownDecoration(
