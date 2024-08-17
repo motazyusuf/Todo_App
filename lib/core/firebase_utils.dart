528055f916cb042419e5d87784daf5639a9e8130
Motaz <motazyusuf@gmail.com>
2024-08-17 16:23:06 +0300

    TaskModel created

--- lib/core/firebase_utils.dart
+++ lib/core/firebase_utils.dart
@@ -3,28 +3,28 @@ import '../models/task_model.dart';
 
 class FirebaseUtils {
 
-  // CollectionReference<TaskModel>getCollection() {
-  //   return FirebaseFirestore.instance
-  //       .collection(TaskModel.collectionName)
-  //       .withConverter<TaskModel>(
-  //           fromFirestore: (snapshot, _) =>
-  //               TaskModel.fromJson(snapshot.data()!),
-  //           toFirestore: (value, _) => value.toJson());
-  // }
+  CollectionReference<TaskModel>getCollection() {
+    return FirebaseFirestore.instance
+        .collection(TaskModel.collectionName)
+        .withConverter<TaskModel>(
+            fromFirestore: (snapshot, _) =>
+                TaskModel.fromJson(snapshot.data()!),
+            toFirestore: (value, _) => value.toJson());
+  }
 
 
-  CollectionReference<TaskModel> getCollection = FirebaseFirestore.instance
-      .collection(TaskModel.collectionName)
-      .withConverter<TaskModel>(
-      fromFirestore: (snapshot, _) =>
-          TaskModel.fromJson(snapshot.data()!),
-      toFirestore: (value, _) => value.toJson());
+  // CollectionReference<TaskModel> getCollection = FirebaseFirestore.instance
+  //     .collection(TaskModel.collectionName)
+  //     .withConverter<TaskModel>(
+  //     fromFirestore: (snapshot, _) =>
+  //         TaskModel.fromJson(snapshot.data()!),
+  //     toFirestore: (value, _) => value.toJson());
 
 
 
   Future<void> addTask (TaskModel task)
   {
-    var collectionRef = getCollection;
+    var collectionRef = getCollection();
     var documentRef = collectionRef.doc();
     return documentRef.set(task);
   }
